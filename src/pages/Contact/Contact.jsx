53d1dc005d66b0354784626c5dfb56423cc65b1f
mdhralif <hasiburrahman999.alif@gmail.com>
2025-06-28 10:48:28 +0600

    contact form modification

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -53,7 +53,7 @@ export default function Contact() {
 
     // Create a new FormData object to send to Web3Forms API
     const form = new FormData();
-    form.append("access_key", ""); // Replace with your Web3Forms access key
+    form.append("access_key", "faffe375-6c79-40d2-98ec-418242568284"); // Replace with your Web3Forms access key
     form.append("name", formData.name);
     form.append("email", formData.email);
     form.append("subject", formData.subject || "New Contact Form Submission");
