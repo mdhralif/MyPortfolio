f4f87ef423c38ddd98dd2b32bb6a155b19b673b4
mdhralif <hasiburrahman999.alif@gmail.com>
2025-06-30 22:45:31 +0600

    .

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -54,13 +54,6 @@ export default function Header() {
             {/* Mobile Menu Button */}
             <div className="flex justify-between items-center md:hidden px-2">
               <Link to="/" className="text-white font-bold text-3xl">αℓιƒ <b style={{color: 'aqua'}}>.</b></Link>
-                            {/* <Link to="/" className="text-gray-300 font-mono text-xs leading-none hover:text-gray-200 transition-colors">
-                <pre className="select-none whitespace-pre">
-{`█▀▀█ █░░ ▀░█▀▀
-█▄▄█ █░░ █ █▀▀
-▀░░▀ ▀▀▀ ▀ ▀░░  `}<span className="text-cyan-400">▀</span>
-                </pre>
-              </Link> */}
               <button 
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
                 className="text-white p-2 transition-all duration-300"
