6554b58b32aa09244c1f9adf1e2fb811228ad667
mdhralif <hasiburrahman999.alif@gmail.com>
2026-02-13 17:26:16 +0600

    refactor: Remove Experience component from main route

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,6 @@ export default function App() {
               <Hero onContactClick={handleContactClick} />
               <Projects />
               <Skills />
-              <Experience />
             </>
           }
         />
