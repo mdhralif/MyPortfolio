276476ffd9131b112a8607c82792b461c70c9a4e
mdhralif <hasiburrahman999.alif@gmail.com>
2026-02-17 23:40:19 +0600

    feat: update year format for consistency in Achievement data

--- src/pages/Achievement/Achievement.jsx
+++ src/pages/Achievement/Achievement.jsx
@@ -5,7 +5,7 @@ const achievements = [
   {
     title: "Banglalink AppQuest",
     org: "Banglalink",
-    year: "December 2025",
+    year: "Dec. 2025",
     description:
       "Built a mobile app using TensorFlow model to guide physiotherapy exercises through phone camera tracking. Integrated Banglalink APPlink API for seamless connectivity.",
     link: "https://www.linkedin.com/posts/md-h-r-alif-7358801a6_honored-to-be-the-champion-at-banglalink-ugcPost-7406008507109478400-oq1J?utm_source=share&utm_medium=member_desktop&rcm=ACoAADAuzBkBSSwMV6Qt1xaULMLxEbtc9MUQ9MU",
