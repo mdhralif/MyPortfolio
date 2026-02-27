3afd77680b9dc109af432da9f70ead4219f58c3c
mdhralif <hasiburrahman999.alif@gmail.com>
2026-02-27 19:19:48 +0600

    hjg

--- src/pages/Projects/FeaturedProjects.jsx
+++ src/pages/Projects/FeaturedProjects.jsx
@@ -104,7 +104,7 @@ export default function FeaturedProjects() {
       <main className="bg-[#161925]" ref={container}>
         <section className="text-white w-full bg-[#161925] pb-12 md:pb-20 pt-20 md:pt-16">
             <h2 className="text-5xl md:text-7xl font-black text-transparent bg-white bg-clip-text text-center leading-tight mb-4 md:mb-8">
-              <span style={{ color: '#2DD4BF' }}>Featured</span> Projects
+              <span style={{ color: '#2DD4BF' }}>Projects</span> I’ve Built
             </h2>       
           {featuredProjects.map((project, i) => {
             const targetScale = 1; // All cards same size
