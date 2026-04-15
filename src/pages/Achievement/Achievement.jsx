81e589400c1c7c3e4578adcaf6118c163cd8a509
mdhralif <hasiburrahman999.alif@gmail.com>
2026-04-15 13:40:52 +0600

    Refactor code structure for improved readability and maintainability

--- src/pages/Achievement/Achievement.jsx
+++ src/pages/Achievement/Achievement.jsx
@@ -2,12 +2,13 @@ import AchievementCarousel from "./AchievementCarousel";
 import banglalinkLogo from "@/assets/images/banglalink.png";
 import gplogo from "@/assets/images/gplogo.png";
 import anthropicLogo from "@/assets/images/Anthropic.jpg";
+import kuetLogo from "@/assets/images/kuet.png";
 
 
 const achievements = [
   {
     certificateName: "Banglalink AppQuest Hackathon",
-    placement: "Champion",
+    placement: "Award of Distinction (Champion)",
     description: "Built a mobile app that uses camera tracking to guide physiotherapy exercises with real-time posture feedback.",
     link: "https://www.linkedin.com/posts/md-h-r-alif-7358801a6_honored-to-be-the-champion-at-banglalink-ugcPost-7406008507109478400-oq1J?utm_source=share&utm_medium=member_desktop&rcm=ACoAADAuzBkBSSwMV6Qt1xaULMLxEbtc9MUQ9MU",
     color: "#fff",
@@ -16,19 +17,28 @@ const achievements = [
   {
     certificateName: "Grameenphone TECH TRENDS",
     placement: "Certificate of Excellence",
-    description: "Recognized for excellent completion of the TECH TRENDS program.",
+    description: "Recognized for successfully completing the TECH TRENDS program, reflecting understanding of emerging technologies and industry innovations.",
     link: null,
     color: "#fff",
     logo: gplogo,
   },
   {
   "certificateName": "AI Fluency: Framework & Foundations",
-  "placement": "Issued by Anthropic",
+  "placement": "Certificate of Completion",
   "description": "Completed a structured program covering AI frameworks, foundational concepts, and best practices for AI fluency.",
   "link": null,
   "color": "#fff",
   "logo": anthropicLogo,
+ },
+ {
+  "certificateName": "KUET Bitfest Hackathon, 2024",
+  "placement": "Recognition of Top 24 Finalist",
+  "description": " Developed an AI-powered Bangla writing platform with translation, AI suggestions, and chatbot support.",
+  "link": null,
+  "color": "#fff",
+  "logo": kuetLogo
 }
+
 ];
 
 export default function Achievement() {
